e0b691ae3e631773315acfb26f169be2caa34aa4
Tymur Rezaei <rezai.timur.04@gmail.com>
2026-09-15 19:39:04 -0700

    removed / in config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Pottery_Sale_Oct/',
+  base: '/Pottery_Sale_Oct',
 })
