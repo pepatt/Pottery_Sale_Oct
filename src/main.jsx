df2327066ea4f11acc148daa4a02246381e14b29
Tymur Rezaei <rezai.timur.04@gmail.com>
2026-09-15 20:47:00 -0700

    changed sass to css

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.scss';
+import './index.css';
 
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
